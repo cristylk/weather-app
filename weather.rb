a7c5e6ec6c6151d53078f9a911a0347533a4951a
cristylk <cristy.koebler@gmail.com>
2014-07-28 15:48:56 -0400

    Weather app first commit

---
+++ weather.rb
@@ -0,0 +1,53 @@
+require 'yahoo_weatherman'
+require 'sinatra'
+
+def weatherman(user_location)
+	client = Weatherman::Client.new
+	celsius_result = client.lookup_by_location(user_location).condition['temp']
+	fahrenheit_result = ((celsius_result * 9) / 5) + 32
+end
+
+def conditions(user_location)
+	client = Weatherman::Client.new
+	client.lookup_by_location(user_location).condition['text']
+end
+
+
+get '/' do
+#	location = params[:location]
+#	your_location = get_location(location)
+#	@message = weatherman(your_location)
+#	@conditions = conditions(your_location)
+#	"The temperature is #{@message} degrees Fahrenheit. It is #{@conditions}"
+	erb :home
+end
+
+post '/weather' do
+	@post = params[:post] ['location']
+
+	@temperature = weatherman(@post)
+	@conditions = conditions(@post)
+
+    "#{@weather}"
+    "#{@conditions}"
+    
+    if (@conditions == 'Sunny' || @conditions == 'Sun')
+        erb :sunny
+    elsif (@conditions == 'Cloudy'|| @conditions == 'Mostly Cloudy' || @conditions == 'Partly Cloudy')
+        erb :cloudy
+    elsif (@conditions == 'Snowy' || @conditions == 'Heavy Snow' || @conditions == 'Blowing Snow')
+        erb :snowy
+    elsif (@conditions == 'Light Rain' || @conditions == 'Showers' || @conditions == 'showers' || @conditions == 'Rain')
+    	erb :rainy
+    else
+        erb :default
+    end
+end
+
+#<div class="cond wc-cloudy wc-icon-32">Cloudy</div>
+#<div class="temp">
+#<span class="f">
+
+
+
+
